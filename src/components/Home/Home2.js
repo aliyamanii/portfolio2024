7b279a7123cb30849edd235bc12a4cb56d9bba3d
aliyamanii <aliyamanii@outlook.com>
2024-05-18 10:33:59 +0330

    [ ADD ] Home components

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.png";
+import Tilt from "react-parallax-tilt";
+import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedinIn, FaTelegramPlane } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET'S GET<span className="purple"> ACQUAINTED </span>
+            </h1>
+            <p className="home-about-body">
+              Skilled software engineer with a year of experience in front-end
+              development, specializing in React and TypeScript. Known for
+              meticulous attention to detail and a strong passion for crafting
+              high-performance user interfaces for web applications using React.
+              <br />
+              <br />I have experience of using <t />
+              <i>
+                <b className="purple">
+                  TypeScript, React.js, Tailwind, Git, Docker{" "}
+                </b>
+              </i>
+              <br />
+              <br />
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/aliyamanii"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://t.me/aliyamaani"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaTelegramPlane />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/aliyamanii"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="mailto:aliyamanii@outlook.com"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <MdEmail />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
